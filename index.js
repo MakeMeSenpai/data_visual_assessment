747063b54dd8c07be598e4495b22e089dbc5c7ba
Anthony <sean.a.protho1@gmail.com>
2021-03-01 12:07:33 -0800

    failed due to categorical complications

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const metaData = document.getElementById("meta-data")
+const perCapita = document.getElementById("per-capita")
+const support = document.getElementById("support")
+
 d3.csv("./data/2019.csv").then((data) => {
     console.log(data)
     console.log(data[0]["Country or region"])
 
-    const metaData = document.getElementById("meta-data")
-
     metaData.innerHTML = `Happiness Data - Year: 2019 - Length: ${data.length}`
+
+    const topTen = data.filter((d,i) => i < 11)
+    perCapita.innerHTML = `${}`
 })
